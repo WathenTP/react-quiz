300149f5e7352f11d2c6f4cedb425bddb5da8af3
wathenTP <thom.wathen@gmail.com>
2019-03-26 20:50:27 +0000

    added componentWillMount, this life cycle is envoked once both on client and server, before initial rendering occurs. This will ensure that when calling setState render will see the updated state and execute only once despite state change. shuffleArray will randomise order of the answer options. The render function has also been updated with the addition of Quiz within the div className=App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Question from './components/Question';
+import quizQuestions from './api/quizQuestions';
 
 class App extends Component {
   constructor(props) {
@@ -20,6 +21,35 @@ class App extends Component {
      result: ''
     };
   }
+  componentWillMount() {
+    const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));  
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0]
+    });
+  }
+  shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  };
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+    if (this.state.questionId < quizQuestions.length) {
+        setTimeout(() => this.setNextQuestion(), 300);
+      } else {
+        // do nothing for now
+      }
+  }
   render () {
     return (
     <div className="App">
@@ -28,10 +58,18 @@ class App extends Component {
         <h2>Thom's Big Fat Quiz of the Year!</h2>
       </div>
       <Question content="Who Killed Anakin Skywalker?" />
+      <Quiz
+          answer={this.state.answer}
+          answerOptions={this.state.answerOptions}
+          questionId={this.state.questionId}
+          question={this.state.question}
+          questionTotal={quizQuestions.length}
+          onAnswerSelected={this.handleAnswerSelected}
+        />
     </div>
     )
-  }
-};
+  };
+}
 
 export default App;
 
