589279a80f921e7a0b3e3307133d8023ad44310a
wathenTP <thom.wathen@gmail.com>
2019-03-26 20:21:50 +0000

    quizQuestions has now been written and provides the literal questions set to appear on the page

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,90 @@
+
+var quizQuestions = [
+    {
+        question: "What faction would you support during the 2nd Galactic Civil War?",
+        answers: [
+            {
+                type: "Galactic Empire",
+                content: "Sith"
+            },
+            {
+                type: "Confederacy",
+                content: "Seperatists"
+            },
+            {
+                type: "Galactic Senate",
+                content: "Jedi"
+            }
+        ]
+    },
+    {
+        question: "Which Hero would you like to fight with?",
+        answers: [
+            {
+                type: "Sith",
+                content: "Emperor Palpatine"
+            },
+            {
+                type: "Seperatists",
+                content: "General Grevious"
+            },
+            {
+                type: "Jedi",
+                content: "Yoda"
+            }
+        ]
+    },
+    {
+        question: "Which of these films is best?",
+        answers: [
+            {
+                type: "Prequel",
+                content: "Revenge of the Sith"
+            },
+            {
+                type: "Original",
+                content: "The Empire Strikes Back"
+            },
+            {
+                type: "Sequel",
+                content: "Don't be stupid"
+            }
+        ]
+    },
+    {
+        question: "Who is the strongest Jedi",
+        answers: [
+            {
+                type: "Swordsman",
+                content: "Mace Windu"
+            },
+            {
+                type: "Force",
+                content: "Yoda"
+            },
+            {
+                type: "Strategist",
+                content: "Obi-Wan Kenobi"
+            }
+        ]
+    },
+    {
+        question: "Why does Ryan Johnson destroy that which is beautiful?",
+        answers: [
+            {
+                type: "Sadomasochism",
+                content: "Killing Snoke off without explanation"
+            },
+            {
+                type: "Mental Retardation",
+                content: "Not providing Rey with a backstory"
+            },
+            {
+                type: "Pychopathy",
+                content: "Not having 1 lightsabre duel"
+            }
+        ]
+    }
+  ];
+  
+  export default quizQuestions;
