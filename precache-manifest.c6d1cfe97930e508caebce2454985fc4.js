16bca192306a95e7ee1cac4ea7a691b052f112ab
wathenTP <thom.wathen@gmail.com>
2019-03-27 20:57:51 +0000

    Updates

---
+++ precache-manifest.c6d1cfe97930e508caebce2454985fc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ab2ae3534517a8067e1e9c087f7c369",
+    "url": "/myapp/static/media/icon-check.8ab2ae35.svg"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "25447d205519620caef6",
+    "url": "/myapp/static/js/main.42e32a98.chunk.js"
+  },
+  {
+    "revision": "3216c62b4a22a2638110",
+    "url": "/myapp/static/js/2.80a7e555.chunk.js"
+  },
+  {
+    "revision": "25447d205519620caef6",
+    "url": "/myapp/static/css/main.6dada2dc.chunk.css"
+  },
+  {
+    "revision": "e8175a59adecaca8d80c583315bb40e2",
+    "url": "/myapp/index.html"
+  }
+];
