5d0d3dd691b5baad5a53ec4314fdd94caf3ccdc2
wathenTP <thom.wathen@gmail.com>
2019-04-01 21:38:43 +0100

    Updates

---
+++ precache-manifest.6c82b0378d86d648bbda0d153f1bbd04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-quiz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ab2ae3534517a8067e1e9c087f7c369",
+    "url": "/react-quiz/static/media/icon-check.8ab2ae35.svg"
+  },
+  {
+    "revision": "260e1d56f90ee7d17fb2",
+    "url": "/react-quiz/static/js/runtime~main.009b0ca2.js"
+  },
+  {
+    "revision": "5e2dfec5eb38bf3471a5",
+    "url": "/react-quiz/static/js/main.5e2f47ed.chunk.js"
+  },
+  {
+    "revision": "3216c62b4a22a2638110",
+    "url": "/react-quiz/static/js/2.80a7e555.chunk.js"
+  },
+  {
+    "revision": "5e2dfec5eb38bf3471a5",
+    "url": "/react-quiz/static/css/main.1709accd.chunk.css"
+  },
+  {
+    "revision": "23b58a14b389b6a3151e3995b014f933",
+    "url": "/react-quiz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.c6d1cfe97930e508caebce2454985fc4.js"
+  "/react-quiz/precache-manifest.6c82b0378d86d648bbda0d153f1bbd04.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/react-quiz/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
